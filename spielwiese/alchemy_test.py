9beb0bcb3e377591510840932084222da2d6099c
Florian Janssens <flo12jan@gmail.com>
2025-09-15 14:02:42 +0200

    Testprogramm hinzugefügt - in spielwiese

---
+++ spielwiese/alchemy_test.py
@@ -0,0 +1,108 @@
+# Testprogramm zum Testen der Datenbankspeicherung
+# ? python.analysis.typeCheckingMode ?
+
+from sqlalchemy import ForeignKey, create_engine
+from sqlalchemy import text
+
+engine = create_engine("sqlite+pysqlite:///:memory:", echo=True)
+
+# "commit as you go"
+with engine.connect() as conn:
+    result = conn.execute(text("CREATE TABLE some_table (x int, y int)"))
+    conn.execute(
+        text("INSERT INTO some_table (x, y) VALUES (:x, :y)"),
+        [{"x": 1, "y": 1}, {"x": 2, "y": 4}],
+    )
+    conn.commit()
+    
+
+# "begin once"
+with engine.begin() as conn:
+    conn.execute(
+        text("INSERT INTO some_table (x, y) VALUES (:x, :y)"),
+        [{"x": 6, "y": 8}, {"x": 9, "y": 10}],
+    )
+
+# Zeilen abrufen
+with engine.connect() as conn:
+    result = conn.execute(text("SELECT x, y FROM some_table"))
+    for row in result:
+        print(f"x: {row.x}  y: {row.y}")
+
+    
+# ORM
+from sqlalchemy.orm import Session
+
+stmt = text("SELECT x, y FROM some_table WHERE y > :y ORDER BY x, y")
+with Session(engine) as session:
+    result = session.execute(stmt, {"y": 6})
+    for row in result:
+        print(f"x: {row.x}  y: {row.y}")
+
+
+with Session(engine) as session:
+    result = session.execute(
+        text("UPDATE some_table SET y=:y WHERE x=:x"),
+        [{"x": 9, "y": 11}, {"x": 13, "y": 15}],
+    )
+    session.commit()
+
+from sqlalchemy import MetaData
+metadata_obj = MetaData()
+
+from sqlalchemy import Table, Column, Integer, String
+# user_table = Table(
+#    "user_account",
+#    metadata_obj,
+#    Column("id", Integer, primary_key=True),
+#    Column("name", String(30)),
+#    Column("fullname", String),
+#)
+
+# from sqlalchemy import ForeignKey
+# address_table = Table(# 
+#    "address",
+#    metadata_obj,
+#    Column("id", Integer, primary_key=True),
+#    Column("user_id", ForeignKey("user_account.id"), nullable=False),
+#    Column("email_address", String, nullable=False),
+#)
+# 
+# metadata_obj.create_all(engine)
+
+# ORM 
+
+from sqlalchemy.orm import DeclarativeBase
+from typing import List, Optional
+from sqlalchemy.orm import Mapped, mapped_column, relationship
+
+class Base(DeclarativeBase):
+    pass
+
+class User(Base):
+    __tablename__ = "user_account"
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+    name: Mapped[str] = mapped_column(String(30))
+    fullname: Mapped[Optional[str]]
+
+    addresses: Mapped[List["Address"]] = relationship(back_populates="user")
+
+    def __repr__(self) -> str:
+        return f"User(id={self.id!r}, name={self.name!r}, fullname={self.fullname!r})"
+
+class Address(Base):
+    __tablename__ = "address"
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+    email_address: Mapped[str]
+    user_id = mapped_column(ForeignKey("user_account.id"),nullable=False)
+
+    user: Mapped[User] = relationship(back_populates="addresses")
+
+    def __repr__(self):
+        return f"Address(id={self.id!r}, email_address={self.email_address!r})"
+    
+Base.metadata.create_all(engine)
+
+    
---
+++ spielwiese/models_test.py
@@ -0,0 +1,189 @@
+from __future__ import annotations
+from sqlalchemy import Integer, String, ForeignKey
+from sqlalchemy import Enum as SQLEnum
+from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column, relationship
+from sqlalchemy.ext.hybrid import hybrid_property
+from typing import List
+from enum import Enum, auto
+from email_validator import validate_email, EmailNotValidError
+from argon2 import PasswordHasher
+from argon2.exceptions import VerifyMismatchError
+from dataclasses import dataclass
+
+# PasswordHasher
+ph = PasswordHasher()
+
+
+# Basisklasse
+class Base(DeclarativeBase):
+    pass
+
+
+class Status(Enum):
+    OFFEN = auto()
+    IN_BEARBEITUNG = auto()
+    ABGESCHLOSSEN = auto()
+
+
+class Student(Base):
+    __tablename__ = "student"
+    id: Mapped[int] = mapped_column(Integer, primary_key=True)
+    _name: Mapped[str] = mapped_column(String)
+    _matrikelnummer: Mapped[str] = mapped_column(String)
+    _email_address: Mapped[str] = mapped_column(String)
+    _password: Mapped[str] = mapped_column("password", String)
+
+    hochschule_id = mapped_column(ForeignKey("hochschule.id"))
+    hochschule: Mapped[Hochschule] = relationship(back_populates="students")
+
+    enrollments: Mapped[List["Enrollment"]] = relationship(back_populates="student")
+    
+    def __init__(self, name: str, matrikelnummer: str, email_address: str, password: str, hochschule: "Hochschule" = None):
+        self.name = name
+        self.matrikelnummer = matrikelnummer
+        self.email_address = email_address
+        self.password = password   # läuft über Setter → Hashing
+        if hochschule:
+            self.hochschule = hochschule
+
+
+    def __repr__(self) -> str:
+        return f"Student: {self.name}"
+    
+    @hybrid_property
+    def name(self):
+        return self._name
+
+    @name.setter
+    def name(self, value: str):
+        self._name = value
+    
+    @hybrid_property
+    def matrikelnummer(self):
+        return self._matrikelnummer
+
+    @matrikelnummer.setter
+    def matrikelnummer(self, matrikelnummer):
+        self._matrikelnummer = matrikelnummer
+    
+    @hybrid_property
+    def hochschule(self):
+        return self.hochschule
+    
+    @hybrid_property
+    def email_address(self):
+        return self._email_address
+    
+    @email_address.setter
+    def email_address(self, value: str):
+        try:
+            new_address = validate_email(value).email
+            self._email_address = new_address
+        except EmailNotValidError as e:
+            raise ValueError(f"Ungültige Email: {e}")
+        
+    @hybrid_property
+    def password(self):
+        raise AttributeError("Passwort ist geschützt")
+
+    @password.setter
+    def password(self, klartext: str) -> None:
+        self._password = ph.hash(klartext)
+        
+    
+    def verify_password(self, passworteingabe: str) -> bool:
+        try:
+            return ph.verify(self._password, passworteingabe)
+        except VerifyMismatchError:
+            return False
+
+
+
+class Hochschule(Base):
+    __tablename__ = "hochschule"
+    id: Mapped[int] = mapped_column(Integer, primary_key=True)
+    _name: Mapped[str] = mapped_column(String)
+
+    students: Mapped[List[Student]] = relationship(back_populates="hochschule")
+
+    def __repr__(self):
+        return f"Hochschule: {self.name}"
+    
+    @hybrid_property
+    def name(self):
+        return self._name
+    
+    @name.setter
+    def name(self, value: str):
+        self._name = value
+
+    @hybrid_property
+    def students(self):
+        return self.students
+
+
+class Kurs(Base):
+    __tablename__ = "kurs"
+    id: Mapped[int] = mapped_column(Integer, primary_key=True)
+    _name: Mapped[str] = mapped_column(String)
+    _nummer: Mapped[str] = mapped_column(String)
+
+    enrollments: Mapped[List["Enrollment"]] = relationship(back_populates="kurs")
+    
+    def __repr__(self) -> str:
+        return f"Kurs: {self.name}"
+    
+    @hybrid_property
+    def name(self):
+        return self._name
+    
+    @name.setter
+    def name(self, value: str):
+        self._name = value
+
+    @hybrid_property
+    def nummer(self):
+        return self._nummer
+    
+    @nummer.setter
+    def nummer(self, value: str):
+        self._nummer = value
+    
+
+class Enrollment(Base):
+    __tablename__ = "enrollment"
+    id: Mapped[int] = mapped_column(Integer, primary_key=True)
+
+    student_id: Mapped[int] = mapped_column(ForeignKey("student.id"))
+    kurs_id: Mapped[int] = mapped_column(ForeignKey("kurs.id"))
+
+    _status: Mapped[Status] = mapped_column(SQLEnum(Status))
+
+    student: Mapped["Student"] = relationship(back_populates="enrollments")
+    kurs: Mapped["Kurs"] = relationship(back_populates="enrollments")
+
+    def __repr__(self) -> str:
+        return f"Kurs: {self.kurs_id}, Status: {self.status.name.replace('_', ' ')}"
+
+    @hybrid_property
+    def status(self):
+        return self._status
+    
+    @status.setter
+    def status(self, neuer_status: Status):
+        self._status = neuer_status
+
+    
+@dataclass
+class EnrollmentDTO:
+    student_name: str
+    kurs_name: str
+    status: str
+
+    @classmethod
+    def make_dto(cls, enrollment: Enrollment) -> "EnrollmentDTO":
+        return cls(
+        student_name=enrollment.student.name,
+        kurs_name=enrollment.kurs.name,
+        status=enrollment.status.name
+    )
